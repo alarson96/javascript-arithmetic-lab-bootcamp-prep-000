a94cd24db9fd949438fe3b19fa28010f353985e5
alarson96@outlook.com <alarson96@outlook.com>
2018-06-16 01:53:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function add()
+{
+  number++
+}
+function subtract()
+{
+  number--
+}
+
+function inc(n)
+{
+  number += n
+}
+
+function dec(n)
+{
+  number -= n
+}
+function multiply()
+{
+  
+}
